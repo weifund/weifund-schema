96af020f369bd2885065721ebb75f5c0c9fb705b
Shahan Khatchadourian <shahan.k.code@gmail.com>
2016-08-22 20:04:23 -0400

    Minor edit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Ajv = require('ajv');
 const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
 const validate = ajv.compile(schema);
 
-var weifundSchemaValidate = function(schemaInput) {
+const weifundSchemaValidate = function(schemaInput) {
     validate(schemaInput);
     return validate;
 };
